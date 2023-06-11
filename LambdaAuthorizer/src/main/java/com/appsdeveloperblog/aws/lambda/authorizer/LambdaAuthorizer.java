e05af09ff7d73bb9d03cf5e02d55badcbb15a874
Armen-02 <armen.aws@protonmail.com>
2023-06-11 08:56:12 -0700

    Add JWT validation

--- LambdaAuthorizer/src/main/java/com/appsdeveloperblog/aws/lambda/authorizer/LambdaAuthorizer.java
+++ LambdaAuthorizer/src/main/java/com/appsdeveloperblog/aws/lambda/authorizer/LambdaAuthorizer.java
@@ -3,18 +3,62 @@ package com.appsdeveloperblog.aws.lambda.authorizer;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.Arrays;
 
 /**
  * Handler for requests to Lambda function.
  */
-public class LambdaAuthorizer implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class LambdaAuthorizer implements RequestHandler<APIGatewayProxyRequestEvent, AuthorizerOutput> {
 
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+    public AuthorizerOutput handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
 
-        
-        return new APIGatewayProxyResponseEvent();
+        String userName = input.getPathParameters().get("userName");
+        String effect = "Allow";
+        String jwt = input.getHeaders().get("Authorization");
+        String region = System.getenv("AWS_REGION");
+        String usersPoolId = System.getenv("PHOTO_APP_USERS_POOL_ID");
+        String audience = System.getenv("PHOTO_APP_USERS_APP_CLIENT_ID");
+
+        JwtUtils jwtUtils = new JwtUtils();
+        DecodedJWT decodedJWT;
+        try {
+            decodedJWT = jwtUtils.validateJwtForUser(jwt, region, usersPoolId, userName, audience);
+            userName = decodedJWT.getSubject();
+        } catch (RuntimeException e) {
+            effect = "Deny";
+            e.printStackTrace();
+        }
+
+        APIGatewayProxyRequestEvent.ProxyRequestContext proxyRequestContext = input.getRequestContext();
+
+        String arn = String.format("arn:aws:execute-api:%s:%s:%s/%s/%s/%s",
+                region,
+                proxyRequestContext.getAccountId(),
+                proxyRequestContext.getApiId(),
+                proxyRequestContext.getStage(),
+                proxyRequestContext.getHttpMethod(),
+                "*");
+
+        Statement statement = Statement.builder()
+                .action("execute-api:Invoke")
+                .effect(effect)
+                .resource(arn)
+                .build();
+
+        PolicyDocument policyDocument = PolicyDocument.builder()
+                .version("2012-10-17")
+                .statements(Arrays.asList(statement))
+                .build();
+
+        AuthorizerOutput authorizerOutput = AuthorizerOutput.builder()
+                .principalId(userName)
+                .policyDocument(policyDocument)
+                .build();
+
+        return authorizerOutput;
     }
 
 }
