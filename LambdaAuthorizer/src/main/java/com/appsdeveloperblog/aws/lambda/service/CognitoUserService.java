2da6b7871c189b0d300e5d8f637e8146141bdb81
Armen-02 <armen.aws@protonmail.com>
2023-06-11 10:08:11 -0700

    CognitoIdentityProviderClient.builder() fails to build

--- LambdaAuthorizer/src/main/java/com/appsdeveloperblog/aws/lambda/service/CognitoUserService.java
+++ LambdaAuthorizer/src/main/java/com/appsdeveloperblog/aws/lambda/service/CognitoUserService.java
@@ -1,16 +1,31 @@
 package com.appsdeveloperblog.aws.lambda.service;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
+
+import com.google.gson.JsonObject;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminGetUserRequest;
+
 
 public class CognitoUserService {
 
-    public JSONPObject getUserByUserName(String userName, String poolId) {
+    // private final CognitoIdentityProviderClient cognitoIdentityProviderClient;
 
-        AdminGetUserRequest adminGetUserRequest = AdminGetUserRequest.builder()
+    public CognitoUserService(String region) {
+        // FIXME builder fails here
+//        this.cognitoIdentityProviderClient = CognitoIdentityProviderClient.builder()
+//                .region(Region.of(region))
+//                .build();
+    }
+
+    public JsonObject getUserByUserName(String userName, String poolId) {
+        JsonObject jsonObject = new JsonObject();
+
+        AdminGetUserRequest adminGetUserRequest = (AdminGetUserRequest) AdminGetUserRequest.builder()
                 .username(userName)
                 .userPoolId(poolId)
                 .build();
-        return null;
+
+        return jsonObject;
     }
 }
